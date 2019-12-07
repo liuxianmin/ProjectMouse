16b592a58c864496c53009ed745deeda1e903184
Xianmin <liuxianmin@hit.edu.cn>
2019-12-07 15:06:33 +0800

    add main files

---
+++ Mouse/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  Mouse
+//
+//  Created by Xianmin Liu on 2019/12/7.
+//  Copyright © 2019年 Xianmin Liu. All rights reserved.
+//
+
+#include <iostream>
+
+class myclass{
+    int i;
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "xianmin hit" << std::endl;
+    std::cout << "Hello, World!\n";
+    return 0;
+}
